b8a4658f41a237ad83fcc7217802ed6aa1ee4e92
goldengrape <github@goldengrape.org>
2023-03-05 21:47:40 -0800

    没输入api_key的时候不应给os env赋值

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,13 @@ st.title("青光眼AI辅助咨询")
 st.write("本系统基于ChatGPT，仅供参考，不作为医疗诊断依据。")
 
 api_key=st.text_input("OpenAI API Key", value="", type="password")
-os.environ["OPENAI_API_KEY"]=api_key
-
 question=st.text_input("请输入您的问题", value="", type="default")
 ask_button=st.button("咨询")
 answer=st.markdown("")
 
 if "index" not in st.session_state:
     if api_key is not None:
+        os.environ["OPENAI_API_KEY"]=api_key
         # 如果index.json不存在，就创建一个
         with st.spinner("正在创建索引，请稍候..."):
             if not os.path.exists("index.json"):
