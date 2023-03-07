f27151d07d199f422c40185266521c8e85219659
goldengrape <github@goldengrape.org>
2023-03-06 16:09:34 -0800

    更换了参考文献，index.json居然小了很多，应该可以用了

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 import streamlit as st
-from gpt_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain import OpenAI
+
 import sys
 import os
 from ingest_data import construct_index
 
-st.title("青光眼AI辅助咨询")
+st.title("青光眼辅助咨询")
 st.write("本系统基于ChatGPT，仅供参考，不作为医疗诊断依据。")
 
-api_key=st.text_input("OpenAI API Key", value="", type="password")
+api_key=st.text_input("请输入OpenAI API Key", value="", type="password")
 question=st.text_input("请输入您的问题", value="", type="default")
 ask_button=st.button("咨询")
 answer=st.markdown("")
 
 if "index" not in st.session_state:
-    if api_key is not None:
+    if api_key != "":
+        from gpt_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+        from langchain import OpenAI
         os.environ["OPENAI_API_KEY"]=api_key
         # 如果index.json不存在，就创建一个
         with st.spinner("正在创建索引，请稍候..."):
